8e356b0ee8d4e08adc1fc35d34f9d3b4b8397674
Javier Molina <javier.molina.sevilla@gmail.com>
2019-12-13 00:30:21 +0100

    refactor: add 'shared' module

--- src/app/department/app-department.module.ts
+++ src/app/department/app-department.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppSharedModule } from '../shared';
 
 import { components } from './components';
 
 @NgModule({
   declarations: [...components],
-  imports: [CommonModule],
+  imports: [CommonModule, AppSharedModule],
   exports: [...components]
 })
 export class AppDepartmentModule {}
