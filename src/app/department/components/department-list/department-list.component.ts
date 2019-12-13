3c5f4fa1ecf68bdfc1b62915cf3cc33dcf61e543
Javier Molina <javier.molina.sevilla@gmail.com>
2019-12-13 01:06:13 +0100

    feat: add new department creation

--- src/app/department/components/department-list/department-list.component.ts
+++ src/app/department/components/department-list/department-list.component.ts
@@ -1,27 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Department } from '../../../core/models';
 import { DepartmentService } from '../../../core/services';
+import { MatDialog } from '@angular/material/dialog';
+import { NewDepartmentDialogComponent } from '../new-department-dialog/new-department-dialog.component';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-department-list',
   templateUrl: './department-list.component.html',
   styleUrls: ['./department-list.component.scss']
 })
-export class DepartmentListComponent implements OnInit {
+export class DepartmentListComponent implements OnInit, OnDestroy {
   departments$: Observable<Department[]>;
   loading$: Observable<boolean>;
+  subs: SubSink;
 
-  constructor(private departmentService: DepartmentService) {
+  constructor(
+    private departmentService: DepartmentService,
+    private dialog: MatDialog
+  ) {
     this.departments$ = departmentService.entities$;
     this.loading$ = departmentService.loading$;
+    this.subs = new SubSink();
   }
 
   ngOnInit() {
     this.departmentService.getAll();
   }
 
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+
   createDepartment() {
-    this.departmentService.add({ id: null, name: 'Nuevo departamento' });
+    this.subs.sink = this.dialog
+      .open(NewDepartmentDialogComponent)
+      .afterClosed()
+      .subscribe(name => {
+        if (name) {
+          this.addDepartment({
+            id: null,
+            name
+          });
+        }
+      });
+  }
+
+  addDepartment(department: Department) {
+    this.departmentService.add(department);
   }
 }
--- src/app/department/components/index.ts
+++ src/app/department/components/index.ts
@@ -1,5 +1,11 @@
 import { DepartmentListComponent } from './department-list/department-list.component';
+import { NewDepartmentDialogComponent } from './new-department-dialog/new-department-dialog.component';
 
-export const components = [DepartmentListComponent];
+export const components = [
+  DepartmentListComponent,
+  NewDepartmentDialogComponent
+];
+export const entryComponents = [NewDepartmentDialogComponent];
 
 export * from './department-list/department-list.component';
+export * from './new-department-dialog/new-department-dialog.component';
