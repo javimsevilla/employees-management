586bc9e20b3d66ee8f8ac21d806c59f8626947a1
Javier Molina <javier.molina.sevilla@gmail.com>
2019-12-13 04:21:36 +0100

    refactor: get api url from environment

--- src/app/store/default-data-service-config.ts
+++ src/app/store/default-data-service-config.ts
@@ -1,6 +1,7 @@
 import { DefaultDataServiceConfig } from '@ngrx/data';
+import { environment } from 'src/environments/environment';
 
 export const defaultDataServiceConfig: DefaultDataServiceConfig = {
-  root: 'http://localhost:3000/',
+  root: environment.apiUrl,
   timeout: 5000 // request timeout
 };
